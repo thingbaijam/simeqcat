7a01ab1612884a36c0bc0817e32c31567d6dd072
kirant <thingbaijam@gmail.com>
2022-11-09 14:40:06 +1300

    sim working

--- simeqcat/mfd.py
+++ simeqcat/mfd.py
@@ -2,25 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 from random import choices, seed
 
-def distr_empirical(M, minimum_mag=None, maximum_mag=None, magnitude_binsize = 0.1, doplot=False):
+# 
+
+def distr_empirical(M, Mmin=None, Mmax=None, magbin = 0.1, doplot=False):
     # a cummulative distrbution
-    if minimum_mag is None:
-        minimum_mag = min(M)
-    if maximum_mag is None:
-        maximum_mag = max(M)
+    if Mmin is None:
+        Mmin = min(M)
+    if Mmax is None:
+        Mmax = max(M)
         
-    mags = np.arange(minimum_mag,\
-                        maximum_mag+magnitude_binsize, \
-                        magnitude_binsize).tolist();
-    N = []
+    mags = np.arange(Mmin,Mmax+magbin, magbin).tolist()
+    
+    N, Mw = [],[]
     for mbin in mags:
         n = sum(m>=mbin for m in M)
-        N.append(n)
+       
+        if n>0:
+           N.append(n)
+           Mw.append(mbin)
+        
     if doplot:
-        plt.semilogy(mags, N, 'o')
+        plt.semilogy(Mw, N, 'o')
         plt.xlabel('Magnitude Mw')
         plt.ylabel('N')
-    return (N, mags)
+        
+    return (N, Mw)
+
 
 def calc_bvalue(mags, magbin = 0.1, Mc=None):
     if Mc is None:
@@ -29,40 +36,31 @@ def calc_bvalue(mags, magbin = 0.1, Mc=None):
     else:
         M = [m for m in mags if m >=Mc] 
     # Aki 1965; Bender 1983; Utsu 1999
-    bvalue = np.log10(np.exp(1))/(np.mean(M) - (Mc-magbin/2.0))
+    bvalue = np.log10(np.exp(1))/(np.mean(M) - (Mc-(magbin/2.0)))
     return bvalue
 
 #
 def calc_avalue(bvalue, M, N):
     # N is number of events with magnitude>=M
-    avalue = np.log10(N)-bvalue*M
-    return avalue
-   
+    a = [np.log10(n)+bvalue*m for m,n in zip(M,N)]
+    return np.mean(a)
+ 
 def sample_GRdistr(bvalue, avalue, Mmin, Mmax, \
                    nevents = 1000, mbin=0.1, \
-                   unround = False,
                    rseed=None):
-    
+    #
     mags = np.arange(Mmin, Mmax+mbin, mbin)
-    
     N = [10**(avalue-bvalue*mw) for mw in mags]
     # weights- is this necessary
     probs = [n/sum(N) for n in N]
     seed(rseed)
     sample_mag = choices(mags, probs, k=nevents)
-    kmags = []
-    # unround 
-    if unround:
-        magerr = np.random.normal(scale=0.05, size=len(sample_mag)).tolist()
-        for m in sample_mag:
-            kmags.append(m + np.random.normal(scale=0.05, size=1)[0])
-    else:
-        kmags = sample_mag    
-    return kmags
+    return sample_mag
 
 
 def test_sample_GRdistr(bvalue, avalue, Mmin, Mmax, nevents=10000, rseed=None, doplot=True):
     #
+    # -- not quite well done!
     samp_mag = sample_GRdistr(bvalue, avalue, Mmin, Mmax,\
                      nevents = nevents, mbin=0.1, rseed=rseed)
     
